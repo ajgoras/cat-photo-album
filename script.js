7090f3c8815e27b6e42dfeee6490cef7ffd11d4e
ajgoras <igorjonczyk99@gmail.com>
2023-02-04 12:06:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let activePhoto;
+
+document.querySelector("#albumdiv").addEventListener("click", () => {
+  document.querySelectorAll(".photo").forEach((el) => {
+    el.classList.remove("activePhoto");
+    el.classList.add("photohover");
+  });
+});
+
+document.querySelectorAll(".photo").forEach((el) => {
+  el.addEventListener("click", function () {
+    setTimeout(() => {
+      el.classList.add("activePhoto");
+      el.classList.remove("photohover");
+      activePhoto = el;
+    }, 1);
+  });
+});
+document.querySelectorAll(".photo").forEach((el) => {
+  el.addEventListener("click", function () {
+    el.classList.add("photohover");
+    if (el.classList.contains("activePhoto")) {
+    } else {
+      el.classList.remove("activePhoto");
+    }
+  });
+});
